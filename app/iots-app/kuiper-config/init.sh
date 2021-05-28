91ed8eb56073ed30223b1dc933127cffc42e6254
komshija <agosone@live.com>
2021-05-28 14:56:56 +0200

    ispravljeno da radi sistem

--- app/iots-app/kuiper-config/init.sh
+++ app/iots-app/kuiper-config/init.sh
@@ -1,8 +1,9 @@
 sleep 15;
 /kuiper/bin/kuiperd &
 sleep 5;
-/kuiper/bin/kuiper create stream co -f /co.txt
-/kuiper/bin/kuiper create rule corule -f /corule.txt
-/kuiper/bin/kuiper create stream no2 -f /no2.txt
-/kuiper/bin/kuiper create rule no2rule -f /no2rule.txt
-wait
+/kuiper/bin/kuiper create stream co '(value float, date string, dataName string) WITH ( DATASOURCE = "device/co/messages", FORMAT = "JSON")';
+/kuiper/bin/kuiper create stream no2 '(value float, date string, dataName string) WITH ( DATASOURCE = "device/no2/messages", FORMAT = "JSON")';
+/kuiper/bin/kuiper create rule corule '{"sql": "SELECT * from co where value > 5", "actions": [ { "log": {} }, { "mqtt": { "server": "mqtt:1883", "topic": "device/co/command" }}]}';
+/kuiper/bin/kuiper create rule no2rule '{"sql": "SELECT * from no2 where value > 150", "actions": [ { "log": {} }, { "mqtt": { "server": "mqtt:1883", "topic": "device/no2/command"}}]}';
+wait;
+

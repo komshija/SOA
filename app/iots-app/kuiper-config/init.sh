c3c1df51a1ae3d0c218112194d0c0abfc97674da
Sokol <stefansokol0999@gmail.com>
2021-06-16 18:54:44 +0200

    Izmenjene vrednosti za analytics

--- app/iots-app/kuiper-config/init.sh
+++ app/iots-app/kuiper-config/init.sh
@@ -2,7 +2,7 @@
 sleep 1;
 /kuiper/bin/kuiper create stream co '(value float, date string, dataName string) WITH ( DATASOURCE = "device/co/messages", FORMAT = "JSON")';
 /kuiper/bin/kuiper create stream no2 '(value float, date string, dataName string) WITH ( DATASOURCE = "device/no2/messages", FORMAT = "JSON")';
-/kuiper/bin/kuiper create rule corule '{"sql": "SELECT * from co where value > 1.0", "actions": [ { "log": {} }, { "mqtt": { "server": "mqtt:1883", "topic": "device/co/command" }}]}';
-/kuiper/bin/kuiper create rule no2rule '{"sql": "SELECT * from no2 where value > 10.0", "actions": [ { "log": {} }, { "mqtt": { "server": "mqtt:1883", "topic": "device/no2/command"}}]}';
+/kuiper/bin/kuiper create rule corule '{"sql": "SELECT * from co where value > 2.0", "actions": [ { "log": {} }, { "mqtt": { "server": "mqtt:1883", "topic": "device/co/command" }}]}';
+/kuiper/bin/kuiper create rule no2rule '{"sql": "SELECT * from no2 where value > 100.0", "actions": [ { "log": {} }, { "mqtt": { "server": "mqtt:1883", "topic": "device/no2/command"}}]}';
 wait;
 
